fedf053e91df1b035c30122a4d7003ce1ad413d4
golovin-igor <igor.i.golovin@gmail.com>
2025-09-20 18:57:41 +0200

    Fix: update using directives in EigrpProtocol and add missing project references in EIGRP project file

--- NetForge.Simulation.Protocols/NetForge.Simulation.Protocols.EIGRP/EigrpProtocol.cs
+++ NetForge.Simulation.Protocols/NetForge.Simulation.Protocols.EIGRP/EigrpProtocol.cs
@@ -1,5 +1,5 @@
 using NetForge.Interfaces.Devices;
-using NetForge.Simulation.Common.Common;
+using NetForge.Interfaces.Events;
 using NetForge.Simulation.Common.Configuration;
 using NetForge.Simulation.Common.Protocols;
 using NetForge.Simulation.DataTypes;
