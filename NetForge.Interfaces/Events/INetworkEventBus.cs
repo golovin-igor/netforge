720282d01659cd7066468288e9c9d99704b6853e
golovin-igor <igor.i.golovin@gmail.com>
2025-09-20 16:00:09 +0200

    Enhance INetworkEventBus with batch publishing, filtering, and metrics tracking

--- NetForge.Interfaces/Events/INetworkEventBus.cs
+++ NetForge.Interfaces/Events/INetworkEventBus.cs
@@ -2,6 +2,14 @@
 
 public interface INetworkEventBus
 {
+    // Core functionality
     void Subscribe<TEventArgs>(Func<TEventArgs, Task> handler) where TEventArgs : NetworkEventArgs;
     Task PublishAsync<TEventArgs>(TEventArgs eventArgs) where TEventArgs : NetworkEventArgs;
+
+    // Enhanced functionality
+    Task PublishBatchAsync<TEventArgs>(IEnumerable<TEventArgs> events) where TEventArgs : NetworkEventArgs;
+    void Subscribe<TEventArgs>(string filter, Func<TEventArgs, Task> handler) where TEventArgs : NetworkEventArgs;
+
+    // Metrics and monitoring
+    EventBusMetrics GetMetrics();
 }
